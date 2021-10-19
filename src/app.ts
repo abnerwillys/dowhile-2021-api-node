6c18aa5f7a639956c8991d53528b4c3fa954a072
Abner Willys <abwillys001@gmail.com>
2021-10-18 22:05:38 -0300

    created routes github

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,19 @@
+import "dotenv/config"
 import express from 'express'
 
 const app = express()
+
+app.get('/github', (request, response) => {
+  return response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  )
+})
+
+app.get('/signin/callback', (request, response) => {
+  const { code } = request.query
+
+  return response.json(code)
+})
+
 
 app.listen(4000, () => console.log(`🚀 Server is online on PORT 4000!`))
