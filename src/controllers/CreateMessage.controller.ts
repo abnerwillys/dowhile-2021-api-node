ef55e4ba97d0e7ce62e43142dc0609ce273ff775
Abner Willys <abwillys001@gmail.com>
2021-10-19 00:07:05 -0300

    logic to create messages

---
+++ src/controllers/CreateMessage.controller.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express'
+import { CreateMessageService } from '../services/CreateMessage.service'
+
+class CreateMessageController {
+  async handle(request: Request, response: Response) {
+    const { message } = request.body
+    const { user_id } = request
+
+    const service = new CreateMessageService()
+    
+    try {
+      const result = await service.execute(message, user_id)
+      return response.json(result)
+
+    } catch (error) {
+      return response.json({ error: error.message})
+    }
+  }
+}
+
+export { CreateMessageController }
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { AuthenticateUserController } from './controllers/AuthenticateUser.controller';
+import { CreateMessageController } from './controllers/CreateMessage.controller';
+import { ensureAuthenticated } from './middlewares/ensureAuthenticated';
 
 const router = Router()
 
 router.post("/authenticate", new AuthenticateUserController().handle)
+router.post("/messages", ensureAuthenticated, new CreateMessageController().handle)
 
 export { router }
